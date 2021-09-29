10c6b7a72998e61c01db3c83c9d402886d3a2d4b
ibobudettak <ibobudettak@yandex.ru>
2021-09-30 00:17:55 +0600

    добавил рандомный выбор продавца и запчати

--- drom_bot.py
+++ drom_bot.py
@@ -1,3 +1,4 @@
+import random
 import selenium
 from selenium import webdriver
 from time import sleep
@@ -5,6 +6,21 @@ from random import randint
 import selenium.webdriver.common.keys
 
 URL = 'https://baza.drom.ru/omsk/sell_spare_parts/'
+
+spare_parts_list = []
+list_of_sellers = []
+
+
+def we_get_the_name_of_the_spare_part():
+    with open('артикул-только-названия.txt', mode='r', encoding='utf 8') as file:
+        for line in file:
+            spare_parts_list.append(line.replace('|', '')[:-1])
+
+
+def get_sellers():
+    with open('пользователи.txt', mode='r', encoding='utf 8') as sellers_file:
+        for sellers in sellers_file:
+            list_of_sellers.append(sellers[:-1])
 
 
 def drom_bot(url):
@@ -18,21 +34,19 @@ def drom_bot(url):
     sleep(randint(3, 5))
     part_name = browser.find_element_by_name('query')
     part_name.clear()
-    part_name.send_keys('Пыльник ШРУСа FB2147')
+    part_name.send_keys(random.choice(spare_parts_list))
     sleep(randint(3, 5))
     part_name.send_keys(selenium.webdriver.common.keys.Keys.ENTER)
     sleep(randint(3, 5))
     value = browser.find_elements_by_class_name('ellipsis-text__left-side')
     for i in value:
-        print(i.text)
-        if str(i.text) == 'AZIMAX':
-            print('да')
+        if str(i.text) == str(random.choice(list_of_sellers)):
             i.click()
             break
-        else:
-            print('нет')
     sleep(5)
     browser.close()
 
 
+we_get_the_name_of_the_spare_part()
+get_sellers()
 drom_bot(url=URL)
