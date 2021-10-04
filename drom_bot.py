eccc4d24e66a1c1cc1d22b1b16c2a88f39f97515
ibobudettak <ibobudettak@yandex.ru>
2021-10-04 20:42:56 +0600

    многопоточность добавил

--- drom_bot.py
+++ drom_bot.py
@@ -4,10 +4,12 @@ from selenium import webdriver
 from time import sleep
 from random import randint
 import selenium.webdriver.common.keys
+import threading
 
 
 spare_parts_list = []
 list_of_sellers = []
+dict_sellers = {}
 
 
 def we_get_the_name_of_the_spare_part():
@@ -23,8 +25,8 @@ def get_sellers():
 
 
 def drom_bot():
-    for cycle in range(1, 31):
-        print(f'{cycle}ый цикл.')
+    for cycle in range(1, 11):
+        print(f'***************  {cycle}ый цикл  ***************')
         browser = webdriver.Edge('D:\PYTHON\Drom_bot\edgedriver_win64\msedgedriver.exe')
         browser.get('https://www.drom.ru/')
         sleep(randint(3, 4))
@@ -42,13 +44,22 @@ def drom_bot():
         value = browser.find_elements_by_class_name('ellipsis-text__left-side')
         for i in value:
             if str(i.text) in list_of_sellers:
+                key = i.text
+                count = 1
+                if key in dict_sellers:
+                    v_dict = dict_sellers[key]
+                    v_dict += 1
+                    dict_sellers[key] = v_dict
+                else:
+                    dict_sellers[key] = count
+
                 print(f'совпадение продавца: {i.text}')
                 i.click()
                 print('Обьявление открыто!')
-                phone = browser.find_element_by_xpath('//*[@id="fieldsetView"]/div/div[1]/'
-                                                      'div/div[3]/div[1]/noindex/div/a')
-                phone.click()
-                print('Телефон просмотрен')
+                # phone = browser.find_element_by_xpath('//*[@id="fieldsetView"]/div/div[1]/'
+                #                                       'div/div[3]/div[1]/noindex/div/a')
+                # phone.click()
+                # print('Телефон просмотрен')
                 break
             else:
                 print('Нет совпал продавец!')
@@ -58,4 +69,15 @@ def drom_bot():
 
 we_get_the_name_of_the_spare_part()
 get_sellers()
-drom_bot()
+
+
+drom_bot_1 = threading.Thread(target=drom_bot)
+drom_bot_2 = threading.Thread(target=drom_bot)
+
+drom_bot_1.start()
+drom_bot_2.start()
+
+drom_bot_1.join()
+drom_bot_2.join()
+
+print(dict_sellers)
